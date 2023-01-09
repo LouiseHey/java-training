0435f02c8ffc31192c236c3ec7e7bb1ffd0929d6
Louise Hey <lhey@scottlogic.com>
2023-01-09 16:43:31 +0000

    fix tests - still need to mock userService

--- src/test/java/com/scottlogic/matcher/controller/OrderControllerIntegrationTest.java
+++ src/test/java/com/scottlogic/matcher/controller/OrderControllerIntegrationTest.java
@@ -23,24 +23,42 @@ import static org.hamcrest.Matchers.hasSize;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class OrderControllerIntegrationTest {
 
+    private static final String AUTH_HEADER = "Authorization";
+
     @LocalServerPort
     private int port;
+
+    private String token;
 
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("username", "Louise");
+        requestParams.put("password", "Password");
+
+        this.token = "Bearer " + given().
+                header("Content-Type", "application/json").
+                body(requestParams.toJSONString()).
+                when().
+                post("/auth/signin").
+                then().
+                statusCode(200).
+                extract().
+                header(AUTH_HEADER);
     }
 
     @Test
     public void whenGetBuyOrders_ThenBuyOrdersReturned() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("userId", "userId");
         requestParams.put("price", 10);
         requestParams.put("quantity", 10);
         requestParams.put("action", "BUY");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams.toJSONString()).
         when().
                 post("/order").
@@ -48,6 +66,8 @@ class OrderControllerIntegrationTest {
                 statusCode(200);
 
         List<OrderDto> orders =
+                given().
+                        header(AUTH_HEADER, token).
                 when().
                         get("/order/buy").
                 then().
@@ -56,7 +76,7 @@ class OrderControllerIntegrationTest {
                         as(new TypeRef<>() {});
 
         assertThat(orders, hasSize(1));
-        assertThat(orders.get(0).getUserId(), equalTo("userId"));
+        assertThat(orders.get(0).getUsername(), equalTo("Louise"));
         assertThat(orders.get(0).getPrice(), equalTo(10));
         assertThat(orders.get(0).getQuantity(), equalTo(10));
         assertThat(orders.get(0).getAction(), equalTo(Action.BUY));
@@ -65,13 +85,13 @@ class OrderControllerIntegrationTest {
     @Test
     public void whenGetSellOrders_ThenSellOrdersReturned() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("userId", "userId");
         requestParams.put("price", 10);
         requestParams.put("quantity", 10);
         requestParams.put("action", "SELL");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams.toJSONString()).
         when().
                 post("/order").
@@ -79,6 +99,8 @@ class OrderControllerIntegrationTest {
                 statusCode(200);
 
         List<OrderDto> orders =
+                given().
+                        header(AUTH_HEADER, token).
                 when().
                         get("/order/sell").
                 then().
@@ -87,7 +109,7 @@ class OrderControllerIntegrationTest {
                         as(new TypeRef<>() {});
 
         assertThat(orders, hasSize(1));
-        assertThat(orders.get(0).getUserId(), equalTo("userId"));
+        assertThat(orders.get(0).getUsername(), equalTo("Louise"));
         assertThat(orders.get(0).getPrice(), equalTo(10));
         assertThat(orders.get(0).getQuantity(), equalTo(10));
         assertThat(orders.get(0).getAction(), equalTo(Action.SELL));
@@ -96,13 +118,13 @@ class OrderControllerIntegrationTest {
     @Test
     public void givenBuyOrderInList_whenPlaceSellOrder_ThenTradeReturned() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("userId", "userId");
         requestParams.put("price", 10);
         requestParams.put("quantity", 10);
         requestParams.put("action", "BUY");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams.toJSONString()).
         when().
                 post("/order").
@@ -110,7 +132,6 @@ class OrderControllerIntegrationTest {
                 statusCode(200);
 
         JSONObject requestParams2 = new JSONObject();
-        requestParams2.put("userId", "userId");
         requestParams2.put("price", 10);
         requestParams2.put("quantity", 10);
         requestParams2.put("action", "SELL");
@@ -118,6 +139,7 @@ class OrderControllerIntegrationTest {
         List<TradeDto> trades =
                 given().
                         header("Content-Type", "application/json").
+                        header(AUTH_HEADER, token).
                         body(requestParams2.toJSONString()).
                 when().
                         post("/order").
@@ -134,13 +156,13 @@ class OrderControllerIntegrationTest {
     @Test
     public void givenNegativeQuantity_whenPlaceOrder_ThenBadRequest() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("userId", "userId");
         requestParams.put("price", 10);
         requestParams.put("quantity", -10);
         requestParams.put("action", "BUY");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams.toJSONString()).
                 when().
                 post("/order").
@@ -148,13 +170,13 @@ class OrderControllerIntegrationTest {
                 statusCode(400);
 
         JSONObject requestParams2 = new JSONObject();
-        requestParams2.put("userId", "userId");
         requestParams2.put("price", 10);
         requestParams2.put("quantity", -10);
         requestParams2.put("action", "SELL");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams2.toJSONString()).
                 when().
                 post("/order").
@@ -165,13 +187,13 @@ class OrderControllerIntegrationTest {
     @Test
     public void givenNegativePrice_whenPlaceOrder_ThenBadRequest() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("userId", "userId");
         requestParams.put("price", -10);
         requestParams.put("quantity", -10);
         requestParams.put("action", "BUY");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams.toJSONString()).
                 when().
                 post("/order").
@@ -179,13 +201,13 @@ class OrderControllerIntegrationTest {
                 statusCode(400);
 
         JSONObject requestParams2 = new JSONObject();
-        requestParams2.put("userId", "userId");
         requestParams2.put("price", -10);
         requestParams2.put("quantity", 10);
         requestParams2.put("action", "SELL");
 
         given().
                 header("Content-Type", "application/json").
+                header(AUTH_HEADER, token).
                 body(requestParams2.toJSONString()).
                 when().
                 post("/order").
