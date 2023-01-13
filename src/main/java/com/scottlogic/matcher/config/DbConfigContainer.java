c9e9f2a05db3b4c08494c0d04c1546a2c4290d00
Louise Hey <lhey@scottlogic.com>
2023-01-13 16:30:17 +0000

    add to docker compose file

--- src/main/java/com/scottlogic/matcher/config/DbConfigContainer.java
+++ src/main/java/com/scottlogic/matcher/config/DbConfigContainer.java
@@ -2,6 +2,8 @@ package com.scottlogic.matcher.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,9 +11,9 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@Profile("!container")
+@Profile("container")
 @EnableMongoRepositories(basePackages = "com.scottlogic.matcher")
-public class DbConfig extends AbstractMongoClientConfiguration {
+public class DbConfigContainer extends AbstractMongoClientConfiguration {
     @Value("${spring.data.mongodb.database}")
     private String databaseName;
 
@@ -21,14 +23,21 @@ public class DbConfig extends AbstractMongoClientConfiguration {
     @Value("${spring.data.mongodb.port}")
     public String port;
 
+    @Value("${spring.data.mongodb.username}")
+    public String mongoUsername = "";
+
+    @Value("${spring.data.mongodb.password}")
+    public String mongoPassword = "";
+
     protected String getDatabaseName() {
         return databaseName;
     }
 
     @Override
     protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        MongoCredential credential = MongoCredential.createCredential(mongoUsername, databaseName, mongoPassword.toCharArray());
         builder
-            .applyConnectionString(new ConnectionString(String.format("mongodb://%s:%s/%s", host, port, databaseName)));
+            .applyConnectionString(new ConnectionString(String.format("mongodb://%s:%s/%s", host, port, databaseName))).credential(credential);
     }
 
     @Override
